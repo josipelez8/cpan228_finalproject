4616fee5b9b4e9ecd1ad8923cc4d640579f5fcfa
josipelez8 <josipelez8@gmail.com>
2026-04-01 17:20:50 -0400

    initial commit

---
+++ src/main/java/com/cpan228/expenselive/ExpenseliveApplication.java
@@ -0,0 +1,13 @@
+package com.cpan228.expenselive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpan228/expenselive/ExpenseliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpan228.expenselive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
